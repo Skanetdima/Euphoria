31dccdad361dad51c1a31eb05698c4a8f9723b5a
dmitry <dimabogacan@gmail.com>
2023-08-28 08:39:07 +0200

    ninth container completed

--- Euphoria/settings.py
+++ Euphoria/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=rmug2#q__$#nyve@d^f7x9i6+(4*9rxwdxu8(3*b_c=07qo0=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Euphoria.urls'
@@ -115,6 +116,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATIC_URL = 'static/'
 
