b95433b33367c361f02de3444bd71e0e9e4dd4d2
dmitry <dimabogacan@gmail.com>
2023-08-28 19:21:41 +0200

    tenth container completed

--- mainpage/static/js/script.js
+++ mainpage/static/js/script.js
@@ -5,9 +5,21 @@ leftBar = document.querySelector('.first_corousel_left_bar');
 rightArrow.addEventListener('click', ArrowHandler);
 leftArrow.addEventListener('click', ArrowHandler);
 
+activeSpan = document.querySelector('.tenth_active_window span:first-child');
+activeSpan.addEventListener('click', spanHandler);
+notActiveSpan = document.querySelector('.not_active_span')
+notActiveSpan.addEventListener('click', spanHandler);
+
+
 function ArrowHandler(e){
     e.preventDefault();
     leftBar.classList.toggle("closed_bar");
     rightBar.classList.toggle("active_bar");
+}
+
+function spanHandler(e){
+    e.preventDefault();
+    activeSpan.classList.toggle("not_active_span");
+    notActiveSpan.classList.toggle("active_span");
 }
 
